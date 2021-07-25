ead4469b572f4c38aa9c737472103fc8248bfb86
Adam Mescher <c-adam.mescher@charter.com>
2021-07-24 18:49:31 -0600

    Add id to genderSchema

--- db/schemas/genderSchema.js
+++ db/schemas/genderSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const genderSchema = new mongoose.Schema({
+  id: { type: Number, require: true },
   name: { type: String, required: true },
   abbreviation: { type: String, required: true },
 });
