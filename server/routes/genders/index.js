cc13cd19cc42e581e37dfa3b19919101e0a8349e
Adam Mescher <c-adam.mescher@charter.com>
2021-08-24 16:33:15 -0600

    Add POST and DELETE endpoints for Gender

--- server/routes/genders/index.js
+++ server/routes/genders/index.js
@@ -27,10 +27,69 @@ router.get('/:id', async (request, response) => {
 
 router.post('/', async (request, response) => {
   try {
-    const { id, name, abbreviation } = request.body;
-    const gender = new Gender({ id, name, abbreviation });
-    const ret = await gender.save();
-    response.status(201).json(ret);
+    const genderBody = request.body;
+    const { id, name, abbreviation } = genderBody;
+    const requiredParameters = ['id', 'name', 'abbreviation'];
+    requiredParameters.forEach((param) => {
+      if (!Object.prototype.hasOwnProperty.call(genderBody, param)) {
+        response
+          .status(422)
+          .json({ error: `You are missing the required '${param}' property` });
+      }
+    });
+    const gender = await new Gender({ id, name, abbreviation });
+    const foundId = await Gender.find({ id });
+    const foundName = await Gender.find({ name });
+    const foundAbbreviation = await Gender.find({ abbreviation });
+    if (foundId.length > 0) {
+      response.status(409).json({
+        message: 'An item with that ID already exists. ID must be unique.',
+      });
+    } else if (foundName.length > 0) {
+      response.status(409).json({
+        message:
+          'An item with that Name already exists. Please use a unique name. ',
+      });
+    } else if (foundAbbreviation.length > 0) {
+      response.status(409).json({
+        message:
+          'An item with that Abbreviation already exists. Please use a unique abbreviation.',
+      });
+    } else {
+      gender.save();
+      response.status(201).json(gender);
+    }
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    response.status(400).end();
+  }
+});
+
+router.delete('/', async (request, response) => {
+  try {
+    await Gender.deleteMany({});
+    response.status(202).json({ message: 'All Genders deleted successfully' });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    response.status(400).end();
+  }
+});
+
+router.delete('/:id', async (request, response) => {
+  try {
+    const toDelete = await Gender.find({ id: request.params.id });
+    if (toDelete.length === 0) {
+      response
+        .status(404)
+        .json({ message: `Gender with ID: ${request.params.id} not found` });
+    } else {
+      await Gender.remove({ id: request.params.id });
+      response.status(202).json({
+        message: `Gender with ID: ${request.params.id} deleted successfully`,
+      });
+    }
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err);
