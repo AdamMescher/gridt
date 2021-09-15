bce13d6462bf0111b54923f779eecccf3a62498c
Adam Mescher <c-adam.mescher@charter.com>
2021-09-14 20:29:02 -0600

    Remove unused components

--- src/components/SimpleSchoolCard/index.jsx
+++
@@ -1,9 +0,0 @@
-const SimpleSchoolCard = ({ school }) => (
-  <li onClick={() => console.log({ school })}>
-    <div>
-      <p>{school.SCH_NAME}</p>
-    </div>
-  </li>
-);
-
-export default SimpleSchoolCard;
