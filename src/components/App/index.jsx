c53339b4e92ae58a82cd21b7bb866d15508f0564
Adam Mescher <c-adam.mescher@charter.com>
2021-10-17 18:54:30 -0600

    App network request on select change WIP

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,17 +1,95 @@
+/* eslint-disable */
 import * as React from 'react';
+import { useApolloClient } from '@apollo/client';
+import { Ring } from 'react-awesome-spinners';
 import AsyncSelectInput from '../AsyncSelectInput';
 import GlobalStyle from '../GlobalStyle';
 import Page from '../Page';
-import Selection from '../Selection';
+import Select from 'react-select';
 import Histogram from '../Histogram';
 import StyledApp from './styled';
 import selectOptions from '../../utils/selectOptions';
+import queries from '../../utils/queries';
 
 const App = () => {
   const [gender, setGender] = React.useState('');
   const [race, setRace] = React.useState('');
   const [disability, setDisability] = React.useState('');
   const [selectedSchool, setSelectedSchool] = React.useState(null);
+  const [graphData, setGraphData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [currentGraph, setCurrentGraph] = React.useState('');
+  const client = useApolloClient();
+  const fetchSchools = async () => {
+    setIsLoading(true);
+    const query = queries[`SCHOOLS_${race.value}_${gender.value}_QUERY`];
+    const { data } = await client.query({
+      query,
+      variables: {
+        schoolsLimit: -1,
+        schoolsFilter: {
+          _operators: {
+            [`RR_${race.value}_${gender.value}_POP`]: {
+              gt: 0,
+            },
+          },
+        },
+      },
+    });
+    let key = Object.keys(data.schools[0])[0];
+    return data ? data.schools.map((school) => ({ x: school[key] })) : [];
+  };
+  React.useEffect(async () => {
+    if (
+      gender &&
+      race &&
+      disability &&
+      selectedSchool &&
+      currentGraph !==
+        `${race.value}_${gender.value}_${disability.value}_${selectedSchool?.COMBOKEY}`
+    ) {
+      setCurrentGraph(
+        `${race.value}_${gender.value}_${disability.value}_${selectedSchool?.COMBOKEY}`,
+      );
+      const schools = await fetchSchools();
+      setGraphData(schools);
+      setIsLoading(false);
+    } else if (
+      gender &&
+      race &&
+      disability &&
+      currentGraph !== `${race.value}_${gender.value}_${disability.value}`
+    ) {
+      const newCurrentGraph = `${race.value}_${gender.value}_${disability.value}`;
+      setCurrentGraph(`${race.value}_${gender.value}_${disability.value}`);
+      const schools = await fetchSchools();
+      setGraphData(schools);
+      setIsLoading(false);
+    } else if (
+      gender &&
+      race &&
+      selectedSchool &&
+      currentGraph !==
+        `${race.value}_${gender.value}_${selectedSchool.COMBOKEY}`
+    ) {
+      console.log('FIRED GENDER RACE SELECTEDSCHOOL');
+      setCurrentGraph(
+        `${race.value}_${gender.value}_${selectedSchool.COMBOKEY}`,
+      );
+      const schools = await fetchSchools();
+      setGraphData(schools);
+      setIsLoading(false);
+    } else if (
+      gender &&
+      race &&
+      currentGraph !== `${race?.value}_${gender?.value}`
+    ) {
+      setCurrentGraph(`${race.value}_${gender.value}`);
+      const schools = await fetchSchools();
+      setGraphData(schools);
+      setIsLoading(false);
+    }
+  });
   return (
     <StyledApp>
       <GlobalStyle />
@@ -20,22 +98,25 @@ const App = () => {
           <div className="select-container">
             <div className="select">
               <h3>Gender</h3>
-              <Selection
+              <Select
                 options={selectOptions.genderOptions}
+                isClearable
                 onChange={setGender}
               />
             </div>
             <div className="select">
               <h3>Race</h3>
-              <Selection
+              <Select
                 options={selectOptions.raceOptions}
+                isClearable
                 onChange={setRace}
               />
             </div>
             <div className="select">
               <h3>Disability</h3>
-              <Selection
+              <Select
                 options={selectOptions.disabilityOptions}
+                isClearable
                 onChange={setDisability}
               />
             </div>
@@ -45,12 +126,7 @@ const App = () => {
             <AsyncSelectInput setSelectedSchool={setSelectedSchool} />
           </div>
           <div className="graph-container">
-            <Histogram
-              selectedSchool={selectedSchool}
-              race={race}
-              gender={gender}
-              disability={disability}
-            />
+            {isLoading ? <Ring /> : <Histogram data={graphData} />}
           </div>
         </div>
       </Page>
--- src/components/Histogram/index.jsx
+++ src/components/Histogram/index.jsx
@@ -1,55 +1,28 @@
 /* eslint-disable */
 import * as React from 'react';
-import { useLazyQuery, gql } from '@apollo/client';
-// import { Ring } from 'react-awesome-spinners';
+import {
+  VictoryChart,
+  VictoryLabel,
+  VictoryBar,
+  VictoryTooltip,
+  VictoryHistogram,
+} from 'victory';
 import StyledHistogram from './styled';
-import queries from '../../utils/queries';
 import generateFill from '../../utils/generateFill';
 
-const Histogram = ({ gender, race, disability, selectedSchool }) => {
-  const fetchSchools = async () => {
-    const query = queries[`SCHOOLS_${race.value}_${gender.value}_QUERY`];
-    const { data, loading } = await useQuery({
-      query,
-      variables: {
-        schoolsLimit: -1,
-        schoolsFilter: {
-          _operators: {
-            [`RR_${race.value}_${gender.value}_POP`]: {
-              gt: 0,
-            },
-          },
-        },
-      },
-    });
-    if (data) {
-      const key = Object?.keys(data?.schools[0])[0];
-      const schools = data.schools.map((school) => ({ x: school[key] }));
-      return schools;
-    }
-    return [];
-  };
+const Histogram = ({ data }) => {
   return (
     <StyledHistogram>
-      {gender?.value && race?.value ? (
-        disability?.value ? (
-          selectedSchool ? (
-            <div>
-              gender: yes, race: yes, disabilty: yes, selectedSchool: yes
-            </div>
-          ) : (
-            <div>
-              gender: yes, race: yes, disability: yes, selectedSchool: no
-            </div>
-          )
-        ) : selectedSchool ? (
-          <div>gender: yes, race: yes, disabilty: no, selectedSchool: yes</div>
-        ) : (
-          <div>gender: yes, race: yes, disability: no, selectedSchool: no</div>
-        )
-      ) : (
-        <div>none</div>
-      )}
+      <VictoryChart>
+        <VictoryHistogram
+          style={{ data: { stroke: 'gray', strokeWidth: 1, fill: 'gray' } }}
+          data={data}
+          bins={[
+            0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25,
+            3.5, 3.75, 4, 4.25, 4.5, 4.75, 5,
+          ]}
+        />
+      </VictoryChart>
     </StyledHistogram>
   );
 };
