885b99ad51a87696a9abea210f4c880a3fde1d10
Adam Mescher <admescher@gmail.com>
2021-10-25 19:16:17 -0600

    Fetch working netlify dev

--- src/functions/apollo-graphql/apollo-graphql.js
+++ src/functions/apollo-graphql/apollo-graphql.js
@@ -1,42 +1,13 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-    allAuthors: [Author!]
-    author(id: Int!): Author
-    authorByName(name: String!): Author
-  }
-  type Author {
-    id: ID!
-    name: String!
-    married: Boolean!
-  }
-`
-
-const authors = [
-  { id: 1, name: 'Terry Pratchett', married: false },
-  { id: 2, name: 'Stephen King', married: true },
-  { id: 3, name: 'JK Rowling', married: false },
-]
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!',
-    allAuthors: () => authors,
-    author: () => {},
-    authorByName: (root, args) => {
-      console.log('hihhihi', args.name)
-      return authors.find((author) => author.name === args.name) || 'NOTFOUND'
-    },
-  },
-}
+const { ApolloServer } = require('apollo-server-lambda');
+const schema = require('../../../server/schema');
+require('../../../server/db');
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  schema,
+  introspection: true,
+  tracing: true,
+});
 
-const handler = server.createHandler()
+const handler = server.createHandler();
 
-module.exports = { handler }
+module.exports = { handler };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './components/App';
-import { store } from './store';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3333/',
+  uri: `/.netlify/functions/apollo-graphql`,
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
