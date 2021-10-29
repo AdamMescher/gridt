cafcb222016407e8e2bf1fbbabaa08d701f6f432
Adam Mescher <admescher@gmail.com>
2021-10-28 22:51:52 -0600

    Fix lint error

--- src/components/Meta/index.jsx
+++ src/components/Meta/index.jsx
@@ -5,11 +5,14 @@ const Meta = () => (
   <Helmet>
     <meta charSet="utf-8" />
     <title>GRID-T</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Work+Sans&display=swap" rel="stylesheet" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Work+Sans&display=swap"
+      rel="stylesheet"
+    />
   </Helmet>
 );
 
