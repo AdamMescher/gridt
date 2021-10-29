fa148cfa72a2620f04cc2dd0024f3999b8892142
Adam Mescher <admescher@gmail.com>
2021-10-28 23:10:06 -0600

    Text align center for header in mobile breakpoints

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -8,14 +8,18 @@ const StyledHeader = styled.header`
   align-items: center;
   font-size: 1.5rem;
   @media (max-width: 1280px) {
+    text-align: center;
     padding-top: 15px;
     font-size: 0.8rem;
   }
-   @media (max-width: 700px) {
-     font-size: 0.6rem;
-   }
+  @media (max-width: 700px) {
+    font-size: 0.6rem;
+  }
   @media (max-width: 625px) {
     font-size: 0.55rem;
+    padding-top: 20px;
+    padding-left: 10px;
+    padding-right: 10px;
   }
 `;
 
