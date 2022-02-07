b58b6eff33841a847b59fbe2a0e6debe99007c3b
Adam Mescher <admescher@gmail.com>
2022-02-07 12:29:00 -0700

    Update races and disabilities seed files to reflect data set

--- server/db/seedFiles/races.js
+++ server/db/seedFiles/races.js
@@ -3,7 +3,7 @@ const races = [
   { id: 1, name: 'asian', abbreviation: 'as' },
   { id: 2, name: 'black', abbreviation: 'bl' },
   { id: 3, name: 'hispanic', abbreviation: 'hi' },
-  { id: 4, name: 'native hawaiian / pacific islander', abbreviation: 'pi' },
+  { id: 4, name: 'native hawaiian / pacific islander', abbreviation: 'hp' },
   { id: 5, name: 'two or more races', abbreviation: 'tr' },
   { id: 6, name: 'white', abbreviation: 'wh' },
 ];
