f67637d7131f1f04726f0c159ed982b317dcc3b2
Adam Mescher <admescher@gmail.com>
2022-02-07 12:30:10 -0700

    Update Stats component with UseMemo dependency array and update styles

--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as d3 from 'd3';
+import StyledStats from './styled';
 import Table from '../Table';
 
 const Stats = ({ data }) => {
@@ -15,60 +16,80 @@ const Stats = ({ data }) => {
     third: d3.quantile(raw, 0.75),
     max: d3.quantile(raw, 1),
   };
-  const columns = React.useMemo(() => [
-    {
-      Header: 'Metric',
-      accessor: 'metric',
-    },
-    {
-      Header: 'Value',
-      accessor: 'value',
-    },
-  ]);
-  const rows = React.useMemo(() => [
-    {
-      metric: 'N',
-      value: data.length,
-    },
-    {
-      metric: 'Mean',
-      value: mean,
-    },
-    {
-      metric: 'Median',
-      value: median,
-    },
-    {
-      metric: 'Mode',
-      value: mode,
-    },
-    {
-      metric: 'Standard Deviation',
-      value: deviation,
-    },
-    {
-      metric: 'IQR Min',
-      value: quartiles.min,
-    },
-    {
-      metric: 'IQR First',
-      value: quartiles.first,
-    },
-    {
-      metric: 'IQR Median',
-      value: quartiles.median,
-    },
-    {
-      metric: 'IQR Third',
-      value: quartiles.third,
-    },
-    {
-      metric: 'IQR Max',
-      value: quartiles.max,
-    },
-  ]);
-
-  return data.length > 1 ? <Table data={rows} columns={columns} /> : null;
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Metric',
+        accessor: 'metric',
+      },
+      {
+        Header: 'Value',
+        accessor: 'value',
+      },
+    ],
+    [],
+  );
+  const rows = React.useMemo(
+    () => [
+      {
+        metric: 'N',
+        value: data.length,
+      },
+      {
+        metric: 'Mean',
+        value: mean,
+      },
+      {
+        metric: 'Median',
+        value: median,
+      },
+      {
+        metric: 'Mode',
+        value: mode,
+      },
+      {
+        metric: 'Standard Deviation',
+        value: deviation,
+      },
+      {
+        metric: 'IQR Min',
+        value: quartiles.min,
+      },
+      {
+        metric: 'IQR First',
+        value: quartiles.first,
+      },
+      {
+        metric: 'IQR Median',
+        value: quartiles.median,
+      },
+      {
+        metric: 'IQR Third',
+        value: quartiles.third,
+      },
+      {
+        metric: 'IQR Max',
+        value: quartiles.max,
+      },
+    ],
+    [
+      data.length,
+      deviation,
+      mean,
+      median,
+      mode,
+      quartiles.first,
+      quartiles.max,
+      quartiles.median,
+      quartiles.min,
+      quartiles.third,
+    ],
+  );
+  return data.length > 1 ? (
+    <StyledStats>
+      <Table data={rows} columns={columns} />
+    </StyledStats>
+  ) : null;
 };
 
 export default Stats;
---
+++ src/components/Stats/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledStats = styled.div`
+    font-size: 12px;
+`;
+
+export default StyledStats;
