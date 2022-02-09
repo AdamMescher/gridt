f829a7bc76855aff9f1d9c7743032736d9661af2
Adam Mescher <admescher@gmail.com>
2022-02-09 16:57:06 -0700

    Resolve rebase commits

--- src/components/About/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-const About = () => (
-  <article>
-    <button>View Data</button>
-  </article>
-);
-
-export default About;
--- src/components/Stats/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const StyledStats = styled.div`
-    font-size: 12px;
-`;
-
-export default StyledStats;
