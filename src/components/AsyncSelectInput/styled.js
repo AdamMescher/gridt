83f154b3fcce571041b37f9c1f2143109761647e
Adam Mescher <admescher@gmail.com>
2022-02-09 14:36:47 -0700

    amend

--- src/components/AsyncSelectInput/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const StyledAsyncSelectInput = styled.div`
-  @media (max-width: 1280px) {
-    font-size: 0.8rem;
-  }
-`;
-
-export default StyledAsyncSelectInput;
--- src/components/Page/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-const StyledPage = styled.div``;
-
-export default StyledPage;
