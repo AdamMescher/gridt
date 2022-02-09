e8d25bd9239df48a95e498e1087c8dbc9122d4ac
Adam Mescher <admescher@gmail.com>
2022-02-08 17:59:22 -0700

    Add Deaf-Blindness graphql queries
    Former-commit-id: aea68f92513d43acb36ee51daa490dbe2158ad5b

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -380,184 +380,366 @@ const queries = {
     }
   `,
   SCHOOLS_AM_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AS_F_POP_DD
       }
     }
   `,
   SCHOOLS_AM_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AM_M_POP_DD
       }
     }
   `,
   SCHOOLS_AS_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AS_F_POP_DD
       }
     }
   `,
   SCHOOLS_AS_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AS_M_POP_DD
       }
     }
   `,
   SCHOOLS_BL_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_BL_F_POP_DD
       }
     }
   `,
   SCHOOLS_BL_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_BL_M_POP_DD
       }
     }
   `,
   SCHOOLS_HI_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HI_F_POP_DD
       }
     }
   `,
   SCHOOLS_HI_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HI_M_POP_DD
       }
     }
   `,
   SCHOOLS_HP_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HP_F_POP_DD
       }
     }
   `,
   SCHOOLS_HP_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HP_M_POP_DD
       }
     }
   `,
   SCHOOLS_TR_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_TR_F_POP_DD
       }
     }
   `,
   SCHOOLS_TR_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_TR_M_POP_DD
       }
     }
   `,
   SCHOOLS_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_WH_F_POP_DD
       }
     }
   `,
   SCHOOLS_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_WH_M_POP_DD
       }
     }
   `,
   SCHOOLS_AM_F_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AM_F_WH_F_DD
       }
     }
   `,
   SCHOOLS_AM_M_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AM_M_WH_M_DD
       }
     }
   `,
   SCHOOLS_AS_F_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AS_F_WH_F_DD
       }
     }
   `,
   SCHOOLS_AS_M_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_AS_M_WH_M_DD
       }
     }
   `,
   SCHOOLS_BL_F_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_BL_F_WH_F_DD
       }
     }
   `,
   SCHOOLS_BL_M_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_BL_M_WH_M_DD
       }
     }
   `,
   SCHOOLS_HI_F_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HI_F_WH_F_DD
       }
     }
   `,
   SCHOOLS_HI_M_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HI_M_WH_M_DD
       }
     }
   `,
   SCHOOLS_HP_F_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HP_F_WH_F_DD
       }
     }
   `,
   SCHOOLS_HP_M_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_HP_M_WH_M_DD
       }
     }
   `,
   SCHOOLS_TR_F_WH_F_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_TR_F_WH_F_DD
       }
     }
   `,
   SCHOOLS_TR_M_WH_M_DD_QUERY: gql`
-    query Quert($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
+    query Query($filter: FilterFindManyDevelopmentaldelayInput, $limit: Int) {
       developmentaldelays(filter: $filter, limit: $limit) {
         RR_TR_M_WH_M_DD
+      }
+    }
+  `,
+  SCHOOLS_AM_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AS_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_AM_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AM_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_AS_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AS_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_AS_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AS_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_BL_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_BL_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_BL_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_BL_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_HI_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HI_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_HI_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HI_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_HP_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HP_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_HP_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HP_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_TR_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_TR_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_TR_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_TR_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_WH_F_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_WH_M_POP_DB
+      }
+    }
+  `,
+  SCHOOLS_AM_F_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AM_F_WH_F_DB
+      }
+    }
+  `,
+  SCHOOLS_AM_M_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AM_M_WH_M_DB
+      }
+    }
+  `,
+  SCHOOLS_AS_F_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AS_F_WH_F_DB
+      }
+    }
+  `,
+  SCHOOLS_AS_M_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_AS_M_WH_M_DB
+      }
+    }
+  `,
+  SCHOOLS_BL_F_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_BL_F_WH_F_DB
+      }
+    }
+  `,
+  SCHOOLS_BL_M_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_BL_M_WH_M_DB
+      }
+    }
+  `,
+  SCHOOLS_HI_F_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HI_F_WH_F_DB
+      }
+    }
+  `,
+  SCHOOLS_HI_M_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HI_M_WH_M_DB
+      }
+    }
+  `,
+  SCHOOLS_HP_F_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HP_F_WH_F_DB
+      }
+    }
+  `,
+  SCHOOLS_HP_M_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_HP_M_WH_M_DB
+      }
+    }
+  `,
+  SCHOOLS_TR_F_WH_F_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_TR_F_WH_F_DB
+      }
+    }
+  `,
+  SCHOOLS_TR_M_WH_M_DB_QUERY: gql`
+    query Query($filter: FilterFindManyDeafblindnessInput, $limit: Int) {
+      developmentaldelays(filter: $filter, limit: $limit) {
+        RR_TR_M_WH_M_DB
       }
     }
   `,
