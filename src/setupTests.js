742699e3e9effbaa3a050dc6f592a5a41801740d
Adam Mescher <admescher@gmail.com>
2022-02-11 17:57:18 -0700

    Fix heroku rebase issues

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { client } from './ApolloClient';
-import { server } from './mocks/server';
-
-beforeAll(async () => server.listen());
-beforeEach(() => client.clearStore());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
