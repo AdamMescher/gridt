c1f68a95c0483708d14c8f68080ef2b174ba3f24
Adam Mescher <admescher@gmail.com>
2022-02-26 17:43:06 -0700

    Add additional globalstyles for project

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -3,11 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
     --button-background: #075985;
     --button-background-hover: #0ea5e9;
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
     body {
       font-family: 'Open Sans', sans-serif;
     }
@@ -63,6 +58,58 @@ const GlobalStyle = createGlobalStyle`
     border-bottom-left-radius: 4px;
     border-top-right-radius: 4px;
   }
+  /*
+  1. Use a more-intuitive box-sizing model.
+*/
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+/*
+  2. Remove default margin
+*/
+* {
+  margin: 0;
+}
+/*
+  3. Allow percentage-based heights in the application
+*/
+html, body {
+  height: 100%;
+}
+/*
+  Typographic tweaks!
+  4. Add accessible line-height
+  5. Improve text rendering
+*/
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+/*
+  6. Improve media defaults
+*/
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+/*
+  7. Remove built-in form typography styles
+*/
+input, button, textarea, select {
+  font: inherit;
+}
+/*
+  8. Avoid text overflows
+*/ƒ
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+/*
+  9. Create a root stacking context
+*/
+#root, #__next {
+  isolation: isolate;
+}
 `;
 
 export default GlobalStyle;
