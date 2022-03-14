abc005350c18f116db91be410013dc54391345dc
Adam Mescher <admescher@gmail.com>
2022-03-14 14:02:03 -0600

    Create GraphSizeErrorMessage component

---
+++ src/components/GraphSizeErrorMessage/index.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { FiAlertTriangle } from 'react-icons/fi';
+
+const GraphSizeErrorMessageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border: 2px solid #fbbf24;
+  border-radius: 4px;
+  max-width: 600px;
+`;
+const IconWrapper = styled.span`
+  margin-left: 8px;
+  display: flex;
+  align-items: center;
+`;
+const ErrorMessageCopyWrapper = styled.div`
+  margin-left: 8px;
+  padding: 4px 8px;
+`;
+const ErrorMessageCopy = styled.p`
+  &:not(:first-child) {
+    margin-top: 4px;
+  }
+`;
+
+const GraphSizeErrorMessage = ({ graphData }) => (
+  <GraphSizeErrorMessageWrapper>
+    <IconWrapper>
+      <FiAlertTriangle size={'2rem'} color="#fbbf24" />
+    </IconWrapper>
+    <ErrorMessageCopyWrapper>
+      <ErrorMessageCopy>
+        Sample size of {graphData.length} is not large enough to generate
+        meaningful data for this subgroup.
+      </ErrorMessageCopy>
+      <ErrorMessageCopy>Please try another combination.</ErrorMessageCopy>
+    </ErrorMessageCopyWrapper>
+  </GraphSizeErrorMessageWrapper>
+);
+
+export default GraphSizeErrorMessage;
