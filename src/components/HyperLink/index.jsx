836edaa6a2d2e01c13b21e947fef8741af91c8bb
Adam Mescher <admescher@gmail.com>
2022-03-14 14:00:03 -0600

    Move HyperLink styles from external file to within HyperLink index file

--- src/components/HyperLink/index.jsx
+++ src/components/HyperLink/index.jsx
@@ -1,10 +1,36 @@
 import * as React from 'react';
-import StyledHyperLink from './styled';
+import styled from 'styled-components';
 
-const HyperLink = (props) => (
-  <StyledHyperLink className="hyperlink" href={props.href} {...props}>
-    {props.text}
-  </StyledHyperLink>
-);
+const HyperLink = (props) => {
+  return (
+    <AnchorTag href={props.href} {...props}>
+      {props.text || props.href}
+    </AnchorTag>
+  );
+};
+
+const AnchorTag = styled.a`
+  --link-color: #0ea5e9;
+  --link-color-hover: #d946ef;
+  --link-color-visited: #818cf8;
+  --link-color-active: #f472b6;
+  color: var(--link-color);
+  text-decoration: none;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  word-break: break-word;
+  hyphens: auto;
+  &:visited {
+    color: var(--link-color-visited);
+  }
+  &:hover {
+    color: var(--link-color-hover);
+    text-decoration: underline;
+  }
+  &:active {
+    color: var(--link-color-active);
+    text-decoration: none;
+  }
+`;
 
 export default HyperLink;
