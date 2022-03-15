0692777fb00e01bc76d14f98f5835ccfe69e1e77
Adam Mescher <admescher@gmail.com>
2022-03-15 15:35:56 -0600

    Move WhatNow styles from WhatNow/styled.js into WhatNow/index.jsx and consume new Citations, OrderedList, and Title components

--- src/components/WhatNow/index.jsx
+++ src/components/WhatNow/index.jsx
@@ -1,43 +1,56 @@
 import * as React from 'react';
-import StyledWhatNow from './styled';
+import styled from 'styled-components';
+import Citations from '../Citations';
+import OrderedList from '../OrderedList';
 import HyperLink from '../HyperLink';
+import Title from '../Title';
 
 const WhatNow = () => {
   return (
-    <StyledWhatNow>
-      <h2 className="title">What Now?</h2>
-      <ol>
-        <li>
+    <Wrapper>
+      <Title.H2 id="what-now">What Now?</Title.H2>
+      <OrderedList>
+        <OrderedList.Item>
           <HyperLink
             href="https://docs.google.com/document/d/1KxDbYr8IV30PubICWSEtScYrlYi8dDyKJew3DUAXWDo/edit?usp=sharing"
             text="Use the linked guide to take preliminary notes on your local
             patterns of disproportionality and make a plan for moving forward."
           />
-        </li>
-        <li>
+        </OrderedList.Item>
+        <OrderedList.Item>
           Explore possible interventions for your patterns of
           disproportionality. Please note that your findings for different
           subgroups might require different actions.
-        </li>
-      </ol>
-      <div>
-        <h3 className="subtitle">Additional Resources</h3>
-        <p className="citation">
-          Munk, T., O'Hara, N., and Sulzberger, L. (2019). Examining
-          representation and identification: Over, under, or both?. (Version
-          2.0). IDEA Data Center. Rockville, MD: Westat.{' '}
-          <HyperLink
-            href="https://ideadata.org/resources/resource/1592/examining-representation-and-identification-over-under-or-both"
-            text="https://ideadata.org/resources/resource/1592/examining-representation-and-identification-over-under-or-both"
-          />
-        </p>
-        <p className="citation">
-          Fergus, E. (2016). Solving disproportionality and achieving equity: A
-          leader's guide to using data to change hearts and minds. Corwin Press.
-        </p>
-      </div>
-    </StyledWhatNow>
+        </OrderedList.Item>
+      </OrderedList>
+      <Resources>
+        <Title.H3>Additional Resources</Title.H3>
+        <ResourcesContent>
+          <Citations>
+            <Citations.Item>
+              <Citations.Citation>
+                Munk, T., O'Hara, N., and Sulzberger, L. (2019). Examining
+                Representation and Identification: Over, Under, or Both?.
+                (Version 2.0). IDEA Data Center. Rockville, MD: Westat.{' '}
+                <HyperLink href="https://ideadata.org/resources/resource/1592/examining-representation-and-identification-over-under-or-both" />
+              </Citations.Citation>
+            </Citations.Item>
+            <Citations.Item>
+              <Citations.Citation>
+                Fergus, E. (2016). Solving Disproportionality and Achieving
+                Equity: A Leader's Guide to Using Data to Change Hearts and
+                Minds. Corwin Press.
+              </Citations.Citation>
+            </Citations.Item>
+          </Citations>
+        </ResourcesContent>
+      </Resources>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section``;
+const Resources = styled.div``;
+const ResourcesContent = styled.div``;
 
 export default WhatNow;
