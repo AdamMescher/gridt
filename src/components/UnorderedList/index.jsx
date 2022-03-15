7444222f29438d17463be51f8e29b5f78ee03afd
Adam Mescher <admescher@gmail.com>
2022-03-15 15:34:10 -0600

    Create UnorderedList component for easily creating an unorded list with common styles within applicatin

---
+++ src/components/UnorderedList/index.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const UnorderedList = ({ children, ...rest }) => (
+  <StyledList {...rest}>{children}</StyledList>
+);
+
+const List = ({ children, ...rest }) => (
+  <StyledList {...rest}>{children}</StyledList>
+);
+const Item = ({ children, ...rest }) => (
+  <StyledItem {...rest}>{children}</StyledItem>
+);
+
+const StyledList = styled.ul`
+  list-style: none;
+  padding-inline-start: 0;
+`;
+const StyledItem = styled.li`
+  margin-top: 4px;
+`;
+
+UnorderedList.List = List;
+UnorderedList.Item = Item;
+
+export default UnorderedList;
