78efd48a9668ea82b14ed963df5ae9e11157ec59
Adam Mescher <admescher@gmail.com>
2022-03-15 15:33:35 -0600

    Create OrderedList component for easily using common styles for all ordered lists

---
+++ src/components/OrderedList/index.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const OrderedList = ({ children, ...rest }) => (
+  <StyledList {...rest}>{children}</StyledList>
+);
+
+const Item = ({ children, ...rest }) => <StyledItem>{children}</StyledItem>;
+
+const StyledList = styled.ol`
+  padding-inline-start: 0;
+  list-style: none;
+  conunter-reset: section;
+`;
+const StyledItem = styled.li`
+  counter-increment: steps;
+  &::before {
+    content: counter(steps);
+    margin-right: 0.3rem;
+    font-weight: bold;
+  }
+`;
+
+OrderedList.Item = Item;
+
+export default OrderedList;
