40c8bd23d751e5b3ae27ef0bad240558f46021f6
Adam Mescher <admescher@gmail.com>
2022-03-16 16:30:40 -0600

    Update Selection component to conditionally render label and add Selection.test.js with 100% code coverage

---
+++ src/components/Selection/Selection.test.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Selection from './index';
+
+describe('Selection', () => {
+  it('renders without crashing', () => {
+    render(
+      <Selection />,
+      <Selection name="one" />,
+      <Selection name="frank" label="label maker" />,
+      <Selection label="Billy Joel" />,
+    );
+  });
+  it('should not render a label if neither name or label prop are provided', () => {
+    render(<Selection />);
+    expect(screen.queryByText('label')).not.toBeInTheDocument();
+  });
+  it('should not render a label if only name prop is provided', () => {
+    render(<Selection name="frank" />);
+    expect(screen.queryByText('frank')).not.toBeInTheDocument();
+  });
+  it('should render a label if only the label prop is provided', () => {
+    render(<Selection label="Sony Music Entertainment" />);
+    expect(screen.queryByText('Sony Music Entertainment')).toBeInTheDocument();
+  });
+});
--- src/components/Selection/index.jsx
+++ src/components/Selection/index.jsx
@@ -1,12 +1,36 @@
-import Select from 'react-select';
+import * as React from 'react';
 import styled from 'styled-components';
+import Select from 'react-select';
+import Spacer from '../Spacer';
 
-const StyledSelect = styled.div``;
-
-const Selection = ({ options, onChange }) => (
-  <StyledSelect>
-    <Select options={options} isClearable onChange={onChange} />
-  </StyledSelect>
+const Selection = ({ name, label, options, onChange }) => (
+  <Wrapper>
+    {label ? (
+      <>
+        <SelectLabel htmlFor={name || label}>{label}</SelectLabel>
+        <Spacer size={2} />
+      </>
+    ) : null}
+    <Select
+      name={name}
+      inputId={name}
+      options={options}
+      isClearable
+      onChange={onChange}
+    />
+  </Wrapper>
 );
+
+const Wrapper = styled.div``;
+const SelectLabel = styled.label`
+  font-weight: 700;
+  display: inline-block;
+  align-self: flex-start;
+  margin-top: 6px;
+  text-align: left;
+  @media (max-width: 800px) {
+    margin-top: 8px;
+  }
+`;
 
 export default Selection;
