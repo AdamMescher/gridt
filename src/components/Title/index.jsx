ecf2264d0a58822b48e726e6202298740b4c1473
Adam Mescher <admescher@gmail.com>
2022-03-16 14:23:22 -0600

    Update Title component to consume only one styled-component and render using as property to simplify

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -2,78 +2,23 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 const Title = ({ level, align, color, size, children, ...rest }) => {
-  let Component;
-  switch (level) {
-    case 1:
-      Component = H1;
-      break;
-    case 2:
-      Component = H2;
-      break;
-    case 3:
-      Component = H3;
-      break;
-    case 4:
-      Component = H4;
-      break;
-    case 5:
-      Component = H5;
-      break;
-    case 6:
-      Component = H6;
-      break;
-    default:
-      Component = H1;
-      break;
-  }
   return (
-    <Component level={level} color={color} align={align} {...rest}>
+    <StyledTitle
+      as={`h${level}` || 1}
+      level={level}
+      color={color}
+      align={align}
+      {...rest}
+    >
       {children}
-    </Component>
+    </StyledTitle>
   );
 };
-const H1 = ({ children, ...rest }) => (
-  <StyledH1 as={'h1'} {...rest}>
-    {children}
-  </StyledH1>
-);
-const H2 = ({ children, ...rest }) => (
-  <StyledH2 as={'h2'} {...rest}>
-    {children}
-  </StyledH2>
-);
-const H3 = ({ children, ...rest }) => (
-  <StyledH3 as={'h3'} {...rest}>
-    {children}
-  </StyledH3>
-);
-const H4 = ({ children, ...rest }) => (
-  <StyledH4 as={'h4'} {...rest}>
-    {children}
-  </StyledH4>
-);
-const H5 = ({ children, ...rest }) => (
-  <StyledH5 as={'h5'} {...rest}>
-    {children}
-  </StyledH5>
-);
-const H6 = ({ children, ...rest }) => (
-  <StyledH6 as={'h6'} {...rest}>
-    {children}
-  </StyledH6>
-);
 
-const Base = styled.span`
+const StyledTitle = styled.span`
   color: ${({ color }) => color || 'inherit'};
   text-align: ${({ align }) => align || 'inherit'};
   font-size: ${({ size }) => (size ? size : null)};
 `;
-
-const StyledH1 = styled(Base)``;
-const StyledH2 = styled(Base)``;
-const StyledH3 = styled(Base)``;
-const StyledH4 = styled(Base)``;
-const StyledH5 = styled(Base)``;
-const StyledH6 = styled(Base)``;
 
 export default Title;
--- src/components/Title/index.test.js
+++ src/components/Title/index.test.js
@@ -6,14 +6,15 @@ import Title from './index';
 describe('Title component', () => {
   it('renders without crashing', () => {
     render(
-      <Title>
+      <Title color={'blue'}>
         <span>renders</span> without crashing
       </Title>,
+      <Title level={1}>This is an h1</Title>,
+      <Title level={2}>This is an h2</Title>,
+      <Title level={3}>This is an h3</Title>,
+      <Title level={4}>This is an h4</Title>,
+      <Title level={5}>This is an h5</Title>,
+      <Title level={6}>This is an h6</Title>,
     );
-  });
-  it('defaults to rendering an h1 tag', () => {
-    render(<Title>Default Title</Title>);
-    const title = screen.getByText('Default Title');
-    console.log({ title });
   });
 });
