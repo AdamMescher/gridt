b5a2c4648ee1f0d728a9d11c85e607a503c9f302
Adam Mescher <admescher@gmail.com>
2022-03-16 14:14:43 -0600

    Update Header component to consume new Title level prop and consume new Spacer component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,19 +2,20 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Meta from '../Meta';
 import Title from '../Title';
+import Spacer from '../Spacer';
 
 const Header = () => (
   <StyledHeader>
     <Meta />
-    <Title.H1>
+    <Title level={1}>
       Gender and Race Intersectional Disproportionality-Tool
-    </Title.H1>
-    <Title.H2>(GRID-T)</Title.H2>
+    </Title>
+    <Spacer size={4} />
+    <Title level={2}>(GRID-T)</Title>
   </StyledHeader>
 );
 
 const StyledHeader = styled.header`
-  margin-top: 20px;
   grid-area: header;
   display: flex;
   flex-direction: column;
