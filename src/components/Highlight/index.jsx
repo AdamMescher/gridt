f76cf6f14aebf438fe0b89e6d690c2c573c8cf90
Adam Mescher <admescher@gmail.com>
2022-03-17 16:15:08 -0600

    Remove now unneeded styled.js file from src/components/Highlight

--- src/components/Highlight/index.jsx
+++ src/components/Highlight/index.jsx
@@ -1,8 +1,68 @@
 import * as React from 'react';
-import StyledHighlight from './styled';
+import styled from 'styled-components';
 
-const Highlight = (props) => {
-  return <StyledHighlight {...props}>{props.text}</StyledHighlight>;
+const Highlight = ({ children, ...rest }) => (
+  <StyledHighlight {...rest}>{children}</StyledHighlight>
+);
+
+const StyledHighlight = styled.span`
+  :root {
+    --slate: #e2e8f0;
+    --dark-slate: #1e293b;
+    --fuchsia: #f5d0fe;
+    --dark-fuchsia: #701a75;
+    --emerald: #6ee7b7;
+    --dark-emerald: #064e3b;
+    --orange: #fdba74;
+    --dark-orange: #7c2d12;
+    --sky: #bae6fd;
+    --dark-sky: #0c4a6e;
+  }
+
+  display: inline-block;
+  padding: 2px 4px;
+  font-size: 18px;
+  border-radius: 4px;
+  color: ${({ color }) => setColor(color)};
+  background: ${({ color }) => setBackground(color)};
+`;
+
+const setColor = (color) => {
+  switch (color) {
+    case 'fuchsia':
+      return 'var(--dark-fuchsia)';
+    case 'emerald':
+      return 'var(--dark-emerald)';
+    case 'orange':
+      return 'var(--dark-orange)';
+    case 'rose':
+      return 'var(--dark-rose)';
+    case 'sky':
+      return 'var(--dark-sky)';
+    case 'slate':
+      return 'var(--dark-slate)';
+    default:
+      return 'var(--dark-slate)';
+  }
+};
+
+const setBackground = (color) => {
+  switch (color) {
+    case 'fuchsia':
+      return 'var(--fuchsia)';
+    case 'emerald':
+      return 'var(--emerald)';
+    case 'orange':
+      return 'var(--orange)';
+    case 'rose':
+      return 'var(--rose)';
+    case 'sky':
+      return 'var(--sky)';
+    case 'slate':
+      return 'var(--slate)';
+    default:
+      return 'var(--slate)';
+  }
 };
 
 export default Highlight;
