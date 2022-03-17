e855a241d26557937c540b2966a7cc4a24b14581
Adam Mescher <admescher@gmail.com>
2022-03-17 16:11:48 -0600

    Update HyperLink component to render children or href value by removing text prop

---
+++ src/components/HyperLink/HyperLink.test.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import HyperLink from './index';
+
+describe('HyperLink Component', () => {
+  it('should render without errors', () => {
+    render(<HyperLink href="https://www.google.com/" />);
+  });
+  it('should render href as link text if no children are provided', () => {
+    const link = render(<HyperLink href="https://www.twitter.com/" />);
+    expect(link.queryByText('https://www.twitter.com/')).toBeInTheDocument();
+  });
+  it('should render link with children if provided', () => {
+    const link = render(
+      <HyperLink href="https://www.espn.com/">
+        this is some link text
+      </HyperLink>,
+    );
+    expect(link.queryByText('this is some link text')).toBeInTheDocument();
+  });
+});
--- src/components/HyperLink/index.jsx
+++ src/components/HyperLink/index.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const HyperLink = (props) => {
+const HyperLink = ({ href, children, ...rest }) => {
   return (
-    <AnchorTag href={props.href} {...props}>
-      {props.text || props.href}
+    <AnchorTag href={href} {...rest}>
+      {children || href}
     </AnchorTag>
   );
 };
