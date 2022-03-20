c478b8d786a6dbe9f93b0d247945a40627d0c8ac
Adam Mescher <admescher@gmail.com>
2022-03-19 22:58:22 -0600

    Update Table component by moving styles to bottom of file and creating Table.test.js file with 100% test coverage

---
+++ src/components/Table/Table.test.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Table from '../Table';
+
+describe('Table Component', () => {
+  it('should render without errors', () => {
+    const columns = [
+      { Header: 'Header One', accessor: 'headerOne' },
+      { Header: 'Header Two', accessor: 'headerTwo' },
+    ];
+    const data = [
+      { headerOne: 1, headerTwo: 2 },
+      { headerOne: 3, headerTwo: 4 },
+    ];
+    render(<Table data={data} columns={columns} />);
+  });
+});
--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -2,6 +2,37 @@ import * as React from 'react';
 import { useTable } from 'react-table';
 import styled from 'styled-components';
 
+const Table = ({ columns, data }) => {
+  const tableInstance = useTable({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+  return (
+    <StyledTable {...getTableProps()}>
+      <thead>
+        {headerGroups?.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows?.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </StyledTable>
+  );
+};
+
 const StyledTable = styled.table`
   border-collapse: collapse;
   td,
@@ -17,36 +48,5 @@ const StyledTable = styled.table`
     font-size: 0.8rem;
   }
 `;
-
-const Table = ({ columns, data }) => {
-  const tableInstance = useTable({ columns, data });
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
-  return (
-    <StyledTable className="table" {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              ))}
-            </tr>
-          );
-        })}
-      </tbody>
-    </StyledTable>
-  );
-};
 
 export default Table;
