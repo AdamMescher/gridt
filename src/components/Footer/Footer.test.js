07b731e3adb8517ecb8c91dcc214dfee5ec2a104
Adam Mescher <admescher@gmail.com>
2022-03-19 22:56:39 -0600

    Update Footer unit test to check for appearance in the DOM

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Footer from './index';
 
 describe('Footer Component', () => {
   it('renders without crashing', () => {
-    render(<Footer />);
+    expect(render(<Footer />).queryByRole('contentinfo')).toBeInTheDocument();
   });
 });
