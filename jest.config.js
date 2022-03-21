b8d0d49813c14694ddc40563973ae507ec7162a0
Adam Mescher <admescher@gmail.com>
2022-03-21 14:21:36 -0600

    Add setupFilesAfterEnv within jest.config.js to execute src/setupTests.js file before Jest test runs

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 const config = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   transform: { '^.+\\.(js|jsx)$': 'babel-jest' },
   moduleNameMapper: {
     d3: '<rootDir>/node_modules/d3/dist/d3.min.js',
