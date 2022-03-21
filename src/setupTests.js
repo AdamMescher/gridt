414913755e33d072c3030e63dbabac8d9803508c
Adam Mescher <admescher@gmail.com>
2022-03-21 14:20:58 -0600

    Remove unused async within beforeAll

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 import { client } from './ApolloClient';
 import { server } from './mocks/server';
 
-beforeAll(async () => server.listen());
+beforeAll(() => server.listen());
 beforeEach(() => client.clearStore());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
