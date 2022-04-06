54afb71d4c0b89651a2fc85668fd7828be8441ee
Adam Mescher <admescher@gmail.com>
2022-04-05 21:36:13 -0600

    Update WhatNow unit test to contain assertion to remove lint warning

--- src/components/WhatNow/WhatNow.test.js
+++ src/components/WhatNow/WhatNow.test.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import WhatNow from './index';
 
 describe('WhatNow Component', () => {
   it('should render without errors', () => {
     render(<WhatNow />);
+    expect(screen.queryByText('What Now?')).toBeInTheDocument();
   });
 });
